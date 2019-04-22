d8ab080272178a72683d30cb38bf9e9a24c04963
arolave <andres@thinkingbytes.net>
2019-04-22 10:50:46 +0300

    ingest server

--- api/src/ingestServer.js
+++ api/src/ingestServer.js
@@ -73,7 +73,7 @@ function buildServer({ swagger = false, port } = {}) {
     reply.send('ok');
 
     cache.set(channel, request.body);
-    forEach(s => s.send(request.body))(getSubscribers(channel));
+    forEach(subscriber => subscriber.send(request.body))(getSubscribers(channel));
   });
 
   process.on('SIGINT', async () => {
