cc7b085f7b9715551081ffd13494d3abee17c7b5
arolave <andres@thinkingbytes.net>
2019-05-07 13:46:55 +0300

    fix lint

---
+++ api/src/websocket-server/socket-handler.js
@@ -0,0 +1,95 @@
+const _ = require('lodash/fp');
+const EventEmitter = require('events');
+const uuidv1 = require('uuid/v1');
+const notFound = require('./messages/notFound');
+const invalid = require('./messages/invalid');
+const ack = require('./messages/ack');
+const failed = require('./messages/failed');
+
+function configureSocketHandler(log, messageHandlerConfigs) {
+  return function socketHandler(ws) {
+    // eslint-disable-next-line no-param-reassign
+    ws.id = uuidv1();
+    // eslint-disable-next-line no-param-reassign
+    ws.lastPing = Date.now();
+
+    ws.on('pong', () => {
+      // eslint-disable-next-line no-param-reassign
+      ws.lastPing = Date.now();
+    });
+
+    // eslint-disable-next-line no-param-reassign
+    ws.messageEmitter = new EventEmitter();
+
+    // eslint-disable-next-line no-param-reassign
+    ws.sendJson = function(val) {
+      log.info({ websocket: ws.id, json: val, type: 'send' });
+      ws.send(JSON.stringify(val));
+    };
+
+    _.forEach(({ messageType, options, fn }) => {
+      ws.messageEmitter.on(messageType, buildMessageHandler(ws, messageType, options, fn));
+    })(messageHandlerConfigs);
+
+    ws.on('message', messageStr => {
+      const message = JSON.parse(messageStr);
+      log.info({ websocket: ws.id, json: message, type: 'recv' });
+      if (message.type == null) {
+        invalid(ws, message, [{ message: `should have required property 'type'` }]);
+        return;
+      }
+
+      if (ws.messageEmitter.listenerCount(message.type) < 1) {
+        notFound(ws, message);
+        return;
+      }
+
+      ws.messageEmitter.emit(message.type, message);
+    });
+
+    ws.on('close', () => {
+      log.info({ websocket: ws.id, type: 'connect' });
+    });
+
+    log.info({ websocket: ws.id, type: 'connect' });
+    return ws;
+  };
+
+  function buildMessageHandler(ws, messageType, { validator, sendAck }, handler) {
+    // eslint-disable-next-line complexity
+    return async message => {
+      try {
+        if (validator && !validator(message)) {
+          invalid(ws, message, validator.errors);
+          return;
+        }
+
+        const response = handler(message, ws);
+
+        if (sendAck) {
+          ack(ws, message);
+        }
+
+        if (response == null) {
+          return;
+        }
+
+        const unwrappedResponse = response.then != null ? await response : response;
+        if (unwrappedResponse == null) {
+          return;
+        }
+
+        if (_.isArray(unwrappedResponse)) {
+          _.forEach(res => ws.sendJson(res), unwrappedResponse);
+          return;
+        }
+
+        ws.sendJson(unwrappedResponse);
+      } catch (error) {
+        failed(ws, message, error);
+      }
+    };
+  }
+}
+
+module.exports = configureSocketHandler;
--- api/src/websocket-server/wrap-websocket-server.js
+++ api/src/websocket-server/wrap-websocket-server.js
@@ -1,11 +1,6 @@
 const _ = require('lodash/fp');
-const EventEmitter = require('events');
-const uuidv1 = require('uuid/v1');
 const Ajv = require('ajv');
-const notFound = require('./messages/notFound');
-const invalid = require('./messages/invalid');
-const ack = require('./messages/ack');
-const failed = require('./messages/failed');
+const configureSocketHandler = require('./socket-handler');
 
 const messageHandlerConfigs = [];
 
@@ -17,7 +12,7 @@ function wrapWebsocketServer(wss, { socketPingRate, socketTimeout, log } = {}) {
     allErrors: true
   });
 
-  const socketHandler = configureSocketHandler(log);
+  const socketHandler = configureSocketHandler(log, messageHandlerConfigs);
 
   wss.on('connection', socketHandler);
 
@@ -48,92 +43,6 @@ function wrapWebsocketServer(wss, { socketPingRate, socketTimeout, log } = {}) {
   };
 
   return wss;
-}
-
-function configureSocketHandler(log) {
-  return function socketHandler(ws) {
-    // eslint-disable-next-line no-param-reassign
-    ws.id = uuidv1();
-    // eslint-disable-next-line no-param-reassign
-    ws.lastPing = Date.now();
-
-    ws.on('pong', () => {
-      // eslint-disable-next-line no-param-reassign
-      ws.lastPing = Date.now();
-    });
-
-    // eslint-disable-next-line no-param-reassign
-    ws.messageEmitter = new EventEmitter();
-
-    // eslint-disable-next-line no-param-reassign
-    ws.sendJson = function(val) {
-      log.info({ websocket: ws.id, json: val, type: 'send' });
-      ws.send(JSON.stringify(val));
-    };
-
-    _.forEach(({ messageType, options, fn }) => {
-      ws.messageEmitter.on(messageType, buildMessageHandler(ws, messageType, options, fn));
-    })(messageHandlerConfigs);
-
-    ws.on('message', messageStr => {
-      const message = JSON.parse(messageStr);
-      log.info({ websocket: ws.id, json: message, type: 'recv' });
-      if (message.type == null) {
-        invalid(ws, message, [{ message: `should have required property 'type'` }]);
-        return;
-      }
-
-      if (ws.messageEmitter.listenerCount(message.type) < 1) {
-        notFound(ws, message);
-        return;
-      }
-
-      ws.messageEmitter.emit(message.type, message);
-    });
-
-    ws.on('close', () => {
-      log.info({ websocket: ws.id, type: 'connect' });
-    });
-
-    log.info({ websocket: ws.id, type: 'connect' });
-    return ws;
-  };
-
-  function buildMessageHandler(ws, messageType, { validator, sendAck }, handler) {
-    // eslint-disable-next-line complexity
-    return async message => {
-      try {
-        if (validator && !validator(message)) {
-          invalid(ws, message, validator.errors);
-          return;
-        }
-
-        const response = handler(message, ws);
-
-        if (sendAck) {
-          ack(ws, message);
-        }
-
-        if (response == null) {
-          return;
-        }
-
-        const unwrappedResponse = response.then != null ? await response : response;
-        if (unwrappedResponse == null) {
-          return;
-        }
-
-        if (_.isArray(unwrappedResponse)) {
-          _.forEach(res => ws.sendJson(res), unwrappedResponse);
-          return;
-        }
-
-        ws.sendJson(unwrappedResponse);
-      } catch (error) {
-        failed(ws, message, error);
-      }
-    };
-  }
 }
 
 module.exports = wrapWebsocketServer;
