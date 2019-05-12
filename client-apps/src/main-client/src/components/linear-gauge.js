895271cb3e9a49eb6ccd781bc14cb7c19604ada9
roeeyud <roeeyud@gmail.com>
2019-05-12 23:01:37 +0300

    CR Fix

--- client-apps/src/main-client/src/components/linear-gauge.js
+++ client-apps/src/main-client/src/components/linear-gauge.js
@@ -4,15 +4,13 @@ import useChannel from '../hooks/use-channel';
 
 export default function LinearGauge(props) {
   const {
-    value: defaultValue,
     min,
     max,
     ...materialProps
   } = props;
   const { value } = useChannel(props);
-  const usedValue = value === null || value === undefined ? defaultValue : value;
   // Calculate the position of the value within a max and min
-  const percentage = (usedValue - min) / (max - min) * 100;
+  const percentage = (value - min) / (max - min) * 100;
 
   return <MaterialLinearGauge {...materialProps} {...data} value={percentage} variant="determinate" />;
 }
