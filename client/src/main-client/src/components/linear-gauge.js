b37c5dcd353a756bbe8bef91febe2cccd0c99a04
roeeyud <roeeyud@gmail.com>
2019-05-19 23:48:17 +0300

    finalized feature fixed bug in linear gauge, Math.isNan is not a function

--- client/src/main-client/src/components/linear-gauge.js
+++ client/src/main-client/src/components/linear-gauge.js
@@ -6,6 +6,6 @@ export default function LinearGauge(props) {
   const { min, max, ...materialProps } = props;
   const { value } = useChannel(props);
   // Calculate the position of the value within a max and min
-  const percentage = String(!Math.isNaN(value) && value ? ((Number(value) - min) / (max - min)) * 100 : 0);
+  const percentage = String(!isNaN(value) && value ? ((Number(value) - min) / (max - min)) * 100 : 0);
   return <MaterialLinearGauge {...materialProps} value={percentage} variant="determinate" />;
 }
